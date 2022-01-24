71cccf0ba64db658f183eeffe5278845fd6b988b
Talhah Awan <talhahawan@gmail.com>
2022-01-23 23:14:01 -0600

    added array function to be called later

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ const weatherCard = document.querySelector('#weather-card');
 const weatherToday = document.querySelector('#weather-today');
 const weatherWeek = document.querySelector('#weather-week');
 const weatherDay = document.querySelector('#weather-day');
+const searcherData = [];
 
 function citySearcher (event) {
+    const searcherInput = document.querySelector('#autocomplete-input');
 
 }
