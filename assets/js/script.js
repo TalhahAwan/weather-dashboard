6a497076e045d94e69ba7bedcd585573608db2ed
Talhah Awan <talhahawan@gmail.com>
2022-01-24 00:04:50 -0600

    trying to fetch api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,17 @@ const weatherToday = document.querySelector('#weather-today');
 const weatherWeek = document.querySelector('#weather-week');
 const weatherDay = document.querySelector('#weather-day');
 const searcherData = [];
+const cityName;
 
 function getTodaysWeather(lat, lon) {
     const apiKey = "5b56094d59883ba5fedee29d1c55668b";
     const apiUrl = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apiKey;
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function(data) {
+                
+            })
+        })
 }
 
 
