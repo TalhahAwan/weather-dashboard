af2e3599a097dc5bcc4e11d9edba3a8134353c0a
Talhah Awan <talhahawan@gmail.com>
2022-01-23 22:56:24 -0600

    created weather and form functions

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const autoCompleteInput = document.querySelector('#autocomplete-input');
+const weatherCard = document.querySelector('#weather-card');
+const weatherToday = document.querySelector('#weather-today');
+const weatherWeek = document.querySelector('#weather-week');
+const weatherDay = document.querySelector('#weather-day');
