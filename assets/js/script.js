e70c6d2a731a676b91b1864539db4eaacc93e138
Talhah Awan <talhahawan@gmail.com>
2022-01-24 00:25:18 -0600

    adding event listener for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const searcherData = [];
 const cityName;
 const area;
 
+searcher.addEventListener('submit', citySearcher);
 function getTodaysWeather(lat, lon) {
     const apiKey = "5b56094d59883ba5fedee29d1c55668b";
     const apiUrl = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apiKey;
