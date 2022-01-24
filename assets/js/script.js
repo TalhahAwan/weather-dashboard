e120b0a9d54a7fa7bdf645a8b112f8e39dcd6dab
Talhah Awan <talhahawan@gmail.com>
2022-01-23 23:42:02 -0600

    put api key and url into get function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const apiKey = "5b56094d59883ba5fedee29d1c55668b";
 const searcher = document.querySelector('#searcher');
 const weatherCard = document.querySelector('#weather-card');
 const weatherToday = document.querySelector('#weather-today');
@@ -7,7 +6,8 @@ const weatherDay = document.querySelector('#weather-day');
 const searcherData = [];
 
 function getTodaysWeather(lat, lon) {
-
+    const apiKey = "5b56094d59883ba5fedee29d1c55668b";
+    const apiUrl = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apiKey;
 }
 
 
