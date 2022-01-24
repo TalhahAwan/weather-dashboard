a4d9f5f8f1d824ca8559b59b1f88de64b2214bd1
Talhah Awan <talhahawan@gmail.com>
2022-01-23 23:29:46 -0600

    added api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const apiKey = "5b56094d59883ba5fedee29d1c55668b";
 const searcher = document.querySelector('#searcher');
 const weatherCard = document.querySelector('#weather-card');
 const weatherToday = document.querySelector('#weather-today');
@@ -5,7 +6,16 @@ const weatherWeek = document.querySelector('#weather-week');
 const weatherDay = document.querySelector('#weather-day');
 const searcherData = [];
 
+function getTodaysWeather(lat, lon) {
+
+}
+
+
+
 function citySearcher (event) {
     const searcherInput = document.querySelector('#autocomplete-input');
+    area = searcherInput.value;
+    if (area) {
 
+    }
 }
