c0c7e22d62e49c9d2758f7f9e2a89452ba3e6e39
Talhah Awan <talhahawan@gmail.com>
2022-01-23 23:10:57 -0600

    added form tag and updated queryselector

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-const autoCompleteInput = document.querySelector('#autocomplete-input');
+const searcher = document.querySelector('#searcher');
 const weatherCard = document.querySelector('#weather-card');
 const weatherToday = document.querySelector('#weather-today');
 const weatherWeek = document.querySelector('#weather-week');
 const weatherDay = document.querySelector('#weather-day');
+
+function citySearcher (event) {
+
+}
